f8181eacedef04967e57776a9c91e54781a8c5be
MiguelSalazar <MiguelSalazarBa@proton.me>
2025-08-23 20:56:53 -0300

    Adicionei uma nav bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,25 @@
-
 import './App.css'
 
 function App() {
   
 
   return (
-    <h1 class= "text-3xl font-bold underline">
-      Hello
+    <>
+    <div class="flex justify-between p-4 bg-gray-200">
+      <ul class="flex gap-3">
+        <button class="rounded-md p-2 bg-green-300">Login</button>
+        <button class="rounded-md">Register</button>
+      </ul>
+      <img src="" alt=" "/>
+      <ul class="flex gap-3">
+        <button class="rounded-md">About</button>
+        <button class="bg-gray-400 p-2 rounded-md color-white">Contact</button>
+      </ul>
+    </div>
+    <h1 class="text-3xl font-bold underline text-center mt-8">
+        Hello World
     </h1>
+    </>
   )
 }
 
