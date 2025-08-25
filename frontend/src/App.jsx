0a4006dc521444b830a9d6508a2da38ef62fbed2
MiguelSalazar <MiguelSalazarBa@proton.me>
2025-08-25 02:29:54 -0300

    mudança do layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,20 +5,27 @@ function App() {
 
   return (
     <>
-    <div class="flex justify-between p-4 bg-gray-200">
-      <ul class="flex gap-3">
-        <button class="rounded-md p-2 bg-green-300">Login</button>
-        <button class="rounded-md">Register</button>
-      </ul>
-      <img src="" alt=" "/>
-      <ul class="flex gap-3">
-        <button class="rounded-md">About</button>
-        <button class="bg-gray-400 p-2 rounded-md color-white">Contact</button>
-      </ul>
-    </div>
-    <h1 class="text-3xl font-bold underline text-center mt-8">
-        Hello World
-    </h1>
+    <body class="bg-white h-screen w-screen">
+      
+      <div class="flex justify-between bg-gray-200 px-20 py-2 z-3">
+        
+        <ul class="flex gap-4">
+          <button class="rounded-md p-2 bg-green-300">Login</button>
+          <button class="rounded-md">Register</button>
+        </ul>
+        
+        <ul class="flex gap-4">
+          <button class="rounded-md">About</button>
+          <button class="bg-gray-400 p-2 rounded-md color-white">Contact</button>
+        </ul>
+
+      </div>
+
+      <div class="flex justify-center bg-gray-100 w-40 h-screen z-2">
+        <div>text</div>
+      </div>
+
+    </body>
     </>
   )
 }
